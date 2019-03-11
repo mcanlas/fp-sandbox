54d6f4489259d6d6e2217f2a1e1d02d3e4cdf36a
Mark Canlas <github@htmlism.com>
2019-03-11 17:03:10 -0400

    add zio main example

---
+++ scalaz-zio/src/main/scala/MyApp.scala
@@ -0,0 +1,23 @@
+import scalaz.zio.App
+import scalaz.zio.console._
+
+object MyApp extends App {
+  def run(args: List[String]) =
+    myAppLogic.either.map(_.fold(_ => 1, _ => 0))
+
+  val myAppLogic =
+    for {
+      _ <- putStrLn("Hello! What is your name?")
+      n <- getStrLn
+      _ <- putStrLn(s"Hello, ${n}, good to meet you!")
+    } yield ()
+}
+
+object UncontrolledMain {
+  def main(args: Array[String]): Unit = {
+    val runtime = new scalaz.zio.DefaultRuntime {}
+    val io = putStrLn("Hello World!")
+
+    runtime.unsafeRun(io)
+  }
+}
