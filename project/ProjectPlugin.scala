c1b2631dd3e824d97fb4aad4014a4cc59fca3256
Mark Canlas <github@htmlism.com>
2022-12-01 20:04:26 -0500

    scala version

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,7 +17,7 @@ object ProjectPlugin extends AutoPlugin {
   override def trigger: PluginTrigger = AllRequirements
 
   override val buildSettings: Seq[Setting[String]] = Seq(
-    scalaVersion := "2.13.8"
+    scalaVersion := "2.13.10"
   )
 
   object ThingsToAutoImport {
